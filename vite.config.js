ac8a8e35c88bfcb1bd1ddf3a1c7f4eb4affad9d7
faseehahmed1 <faseeh_455@hotmail.com>
2023-09-10 10:01:41 +0100

    base url added to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/manage_landing_page_frontend_mentors",
   plugins: [react()],
   server: {
     port: 8000,
